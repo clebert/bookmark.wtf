d2c6365e5efaa053531b951736335dabd3116d56
Clemens Akens <clebert@me.com>
2020-11-26 18:07:01 +0100

    Add README and improve landing page

---
+++ README.md
@@ -0,0 +1,29 @@
+# bookmark.wtf
+
+An open source developed bookmark manager with GitHub Gist as data backend.
+
+1. To use this application, you need to sign in to GitHub and give it permission
+   to access your gists.
+1. The application does not track you or store data outside your browser or
+   GitHub. The source code of the application is publicly available.
+1. Your bookmarks are stored in markdown format (human and machine readable) in
+   GitHub Gist. This allows you to use them without this application.
+1. You can manage as many gists and thus bookmark collections as you like.
+1. By sharing the URL, other people can access your bookmark collections. Your
+   bookmark collections, respectively your gists, are only "secret" as long as
+   you do not share the URL with anyone.
+
+**Disclaimer:** This application is developed by me in my spare time for free.
+You have no claim to correct functioning or even availability. Please use this
+application at your own risk. There is an explicit risk that due to software
+bugs your data on GitHub Gist may be lost.
+
+## Rationale
+
+I implemented this application mainly as a basis for experimenting with software
+architecture design patterns. The application offers a manageable but
+sufficiently complex functional range and was implemented by me using
+state-of-the-art technology.
+
+At the same time, I wanted a simple bookmark manager for all devices and
+browsers. Therefore, working on this project is a win-win situation for me.
--- src/components/screens/unauthorized-screen.tsx
+++ src/components/screens/unauthorized-screen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BulmaContent} from '../../bulma/bulma-content';
 import {BulmaHero} from '../../bulma/bulma-hero';
 import {BulmaLevel} from '../../bulma/bulma-level';
 import {BulmaTitle} from '../../bulma/bulma-title';
@@ -22,12 +23,47 @@ export function UnauthorizedScreen({
         items={[<AppName />, <SignInButton authState={authState} />]}
       />
 
-      <BulmaHero color="info" size="medium" isBold>
+      <BulmaHero color="dark" size="medium" isBold>
         <BulmaTitle size="3">Manage your bookmarks</BulmaTitle>
 
         <BulmaTitle size="4" isSubtitle>
           with <a href="https://gist.github.com">GitHub Gist</a> as data backend
         </BulmaTitle>
+      </BulmaHero>
+
+      <BulmaHero color="info" isBold>
+        <BulmaContent size="small">
+          <ol>
+            <li>
+              To use this application, you need to sign in to GitHub and give it
+              permission to access your gists.
+            </li>
+
+            <li>
+              The application does not track you or store data outside your
+              browser or GitHub. The{' '}
+              <a href="https://github.com/clebert/bookmark.wtf">source code</a>{' '}
+              of the application is publicly available.
+            </li>
+
+            <li>
+              Your bookmarks are stored in markdown format (human and machine
+              readable) in GitHub Gist. This allows you to use them without this
+              application.
+            </li>
+
+            <li>
+              You can manage as many gists and thus bookmark collections as you
+              like.
+            </li>
+
+            <li>
+              By sharing the URL, other people can access your bookmark
+              collections. Your bookmark collections, respectively your gists,
+              are only "secret" as long as you do not share the URL with anyone.
+            </li>
+          </ol>
+        </BulmaContent>
       </BulmaHero>
     </>
   );
