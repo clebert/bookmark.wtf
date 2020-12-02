a1627d57f8a2d9380bb3465faa159a6a7a8fa1a3
Clemens Akens <clebert@me.com>
2020-12-02 17:41:19 +0100

    Update loxia

--- src/hooks/use-sender.ts
+++ src/hooks/use-sender.ts
@@ -2,7 +2,7 @@ import {createSenderHook} from 'loxia';
 import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 
 export const useSender = createSenderHook({
-  useCallback: useCallback as any, // TODO
+  useCallback,
   useEffect,
   useMemo,
   useRef,
