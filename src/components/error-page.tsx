81faf64b7f1843ee063a6c8ff6425f9942ac21c5
Clemens Akens <clebert@me.com>
2021-03-02 18:23:31 +0100

    Improve error page design and add reload button

--- src/components/error-page.tsx
+++ src/components/error-page.tsx
@@ -1,25 +1,40 @@
 import {JSX, h} from 'preact';
+import {useCallback} from 'preact/hooks';
 import {join} from '../utils/join';
+import {Theme} from '../utils/theme';
+import {Button} from './button';
+import {Icon} from './icon';
 import {Page} from './page';
 import {Topbar} from './topbar';
+import {TopbarItem} from './topbar-item';
 
 export function ErrorPage(): JSX.Element {
+  const reload = useCallback(() => window.location.reload(), []);
+
   return (
     <Page>
       <Topbar>
-        <h2
-          class={join([
-            'text-red-600',
-            'text-2xl',
-            'font-light',
-            'whitespace-nowrap',
-            'cursor-default',
-            'select-none',
-          ])}
-        >
-          Oops, an error occurred!
-        </h2>
+        <TopbarItem>
+          <Button onClick={reload}>
+            <Icon type="refresh" />
+            Reload
+          </Button>
+        </TopbarItem>
       </Topbar>
+
+      <span
+        class={join([
+          Theme.danger().textColor,
+          Theme.normalFont,
+          'border',
+          'border-transparent',
+          'cursor-default',
+          'select-none',
+          'px-2',
+        ])}
+      >
+        Oops, an error occurred!
+      </span>
     </Page>
   );
 }
--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -10,6 +10,7 @@ export interface IconProps {
     | 'login'
     | 'logout'
     | 'pencil'
+    | 'refresh'
     | 'trash'
     | 'x';
 }
@@ -52,6 +53,10 @@ export function Icon({type}: IconProps): JSX.Element {
 
         {type === 'pencil' && (
           <path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+        )}
+
+        {type === 'refresh' && (
+          <path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
         )}
 
         {type === 'trash' && (
