2d2ab324361ab19e47ba8c457eabd6663693d647
Clemens Akens <clebert@me.com>
2021-03-10 20:45:36 +0100

    Update Sonnar to v0.2.0

--- src/e2e-tests/has-text.ts
+++ src/e2e-tests/has-text.ts
@@ -1,5 +1,8 @@
 import {Discriminator, pseudoClass} from 'sonnar';
 
+/**
+ * https://playwright.dev/docs/selectors#text-selector
+ */
 export function hasText(value: string): Discriminator {
   return pseudoClass(`:has-text(${JSON.stringify(value)})`);
 }
