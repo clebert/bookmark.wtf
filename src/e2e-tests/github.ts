310aff29a7d38360fd956fea7287331325ae90d8
Clemens Akens <clebert@me.com>
2023-01-30 23:38:02 +0100

    Fix e2e tests

--- src/e2e-tests/github.ts
+++ src/e2e-tests/github.ts
@@ -7,5 +7,5 @@ export const github = {
   loginField: page.path(any().filter(attribute(`#login_field`))),
   passwordField: page.path(any().filter(attribute(`#password`))),
   primaryButton: page.path(any().filter(attribute(`.btn-primary`))),
-  otpField: page.path(any().filter(attribute(`#otp`))),
+  otpField: page.path(any().filter(attribute(`#totp`))),
 };
