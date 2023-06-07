32bb904a9e393be2385cf2989765f3bec2b5e679
Clemens Akens <clebert@me.com>
2023-06-07 09:44:56 +0200

    Update onecfg and dependencies

--- src/utils/create-github-client.ts
+++ src/utils/create-github-client.ts
@@ -1,8 +1,9 @@
-import {Client} from '@urql/core';
+import {Client, cacheExchange, fetchExchange} from '@urql/core';
 
 export function createGithubClient(token: string): Client {
   return new Client({
     url: `https://api.github.com/graphql`,
     fetchOptions: {headers: {authorization: `token ${token}`}},
+    exchanges: [cacheExchange, fetchExchange],
   });
 }
