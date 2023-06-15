394298541d25e3bfc1aa603586a4a6369be20cfb
Clemens Akens <clebert@me.com>
2023-06-15 14:07:25 +0200

    Remove unnecessary `completeAuthorization` call

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import {App} from './components/app.js';
-import {completeAuthorization} from './utils/complete-authorization.js';
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
 import 'tailwindcss/tailwind.css';
@@ -7,7 +6,5 @@ import 'tailwindcss/tailwind.css';
 window.addEventListener(`popstate`, () => {
   location.reload();
 });
-
-completeAuthorization();
 
 createRoot(document.querySelector(`main#app`)!).render(<App />);
